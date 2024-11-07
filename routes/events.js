16ee6ce1c699f1635c9b7c5f89dc9890a15c99d5
Fadd52 <lucas11091109@gmail.com>
2024-11-07 13:07:40 +0200

    Create event feature

--- routes/events.js
+++ routes/events.js
@@ -5,4 +5,7 @@ const eventController = require('../controllers/eventController');
 // GET /events - Get all events
 router.get('/', eventController.getAllEvents);
 
+// Post/events - Create event
+router.post('/', eventController.createEvent);
+
 module.exports = router;
