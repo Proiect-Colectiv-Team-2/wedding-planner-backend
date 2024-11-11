faaf546eba21ad1cd48eaf34526a2641aa6c9cc3
vasiu <e12125518@student.tuwien.ac.at>
2024-11-11 19:12:22 +0200

    add participant routes

--- routes/events.js
+++ routes/events.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const eventController = require('../controllers/eventController');
+const participantController = require("../controllers/participantController");
 
 // GET /events - Get all events
 router.get('/', eventController.getAllEvents);
@@ -13,5 +14,12 @@ router.put('/:id', eventController.updateEvent);
 
 // DELETE /events - Delete event
 router.delete('/:id', eventController.deleteEvent);
+
+
+router.post("/:eventId/participants", participantController.createParticipant);
+
+router.get("/:eventId/participants", participantController.getParticipants);
+
+router.delete("/:eventId/participants/:userId", participantController.removeParticipant);
 
 module.exports = router;
