d5929bd7601378aa0833fa7ca791c8f623e8b6d9
vasiu <e12125518@student.tuwien.ac.at>
2024-11-11 18:58:18 +0200

    implement getParticipant controller

--- controllers/participantController.js
+++ controllers/participantController.js
@@ -31,4 +31,22 @@ exports.createParticipant = async (req, res, next) => {
     catch(err){
         res.status(500).json({message: "Error: ", err});
     }
-}                                     
+} 
+
+exports.getParticipants = async (req, res, next) => {
+    try
+   { const eventId = req.params.eventId;
+
+    const event = await Event.findById(eventId).populate("invitations", "email firstname lastname");
+
+    if(!event){
+        return res.status(404).json({message: "Event not found"});
+    }
+
+    res.status(200).json({participants: event.invitations});
+
+    }
+    catch(err){
+        res.status(500).json({message: "Error: ", err});
+    }
+}
