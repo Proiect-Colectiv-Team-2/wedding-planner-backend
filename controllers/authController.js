025b1dbecbc608514d0c699cfd80887bbb166779
Janos Stelli <stellijanos23@gmail.com>
2024-11-11 09:07:29 +0200

    implement user login with jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -54,6 +54,64 @@ const signup = async (req, res, next) => {
     } catch (err) {
         return res.status(500).json({ message: 'Signing up failed, please try again.' });
     }
+
+    res.status(201).json({
+        user: newUser.id,
+        email: newUser.email,
+        token
+    });
+}
+
+
+const login = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    let existingUser;
+
+    try {
+        existingUser = await User.findOne({ email });
+    } catch (err) {
+        return res.status(500).json({ message: 'Login failed, please try again later.' });
+    }
+
+    if (!existingUser) {
+        return res.status(500).json({ message: 'Invalid credentials provided.' });
+    }
+
+    let isValidPassword = false;
+    try {
+        isValidPassword = await bcrypt.compare(password, existingUser.password);
+    } catch (err) {
+        return res.status(500).json({ message: 'Login failed plaese check your credentials' });
+    }
+
+    if (!isValidPassword) {
+        return res.status(500).json({ message: 'Invalid credentials provided.' });
+    }
+
+
+    let token;
+    try {
+        token = jwt.sign(
+            {
+                userId: existingUser.id,
+                email: existingUser.email
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: '1h'
+            }
+        );
+    } catch (err) {
+        return res.status(500).json({ message: 'Logging in failed, please try again' });
+    }
+
+    res.json({
+        userId: existingUser.id,
+        email: existingUser.email,
+        token
+    });
 }
 
 exports.signup = signup;
+exports.login = login;
