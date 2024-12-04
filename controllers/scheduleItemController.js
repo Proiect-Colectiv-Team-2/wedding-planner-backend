f0765efe64d94440853b1985dadfa721861d3d11
vasiu <e12125518@student.tuwien.ac.at>
2024-12-04 13:46:09 +0200

    implement update schedule item

--- controllers/scheduleItemController.js
+++ controllers/scheduleItemController.js
@@ -40,3 +40,33 @@ exports.createScheduleItem = async (req, res) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 }
+
+// Update schedule item
+exports.updateScheduleItem = async (req, res) => {
+    try {
+        const scheduleItemId = req.params.id;
+
+        const scheduleItem = await ScheduleItem.findById(scheduleItemId);
+
+        if (!scheduleItem) {
+            return res.status(404).json({ message: "Schedule item not found" });
+        }
+
+        const { title, description, startTime, endTime } = req.body;
+
+        scheduleItem.title = title || scheduleItem.title;
+        scheduleItem.description = description || scheduleItem.description;
+        scheduleItem.startTime = startTime || scheduleItem.startTime;
+        scheduleItem.endTime = endTime || scheduleItem.endTime;
+
+        await scheduleItem.save();
+
+        res.status(200).json({
+            message: 'Schedule item successfully updated',
+            data: { scheduleItem }
+        });
+    }
+    catch(err){
+        res.status(500).json({message: 'Internal server error'});
+    }
+}
