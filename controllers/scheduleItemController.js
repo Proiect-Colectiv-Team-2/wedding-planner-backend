b644ba5d163d1027d8e83d8414a47b1d5fa99765
vasiu <e12125518@student.tuwien.ac.at>
2024-12-04 13:28:37 +0200

    implement create schedule item

--- controllers/scheduleItemController.js
+++ controllers/scheduleItemController.js
@@ -1,4 +1,5 @@
 const ScheduleItem = require('../models/ScheduleItem');
+const Event = require('../models/Event')
 
 // Get all schedule items
 exports.getAllScheduleItems = async (req, res) => {
@@ -10,3 +11,32 @@ exports.getAllScheduleItems = async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 };
+
+// Create schedule item
+exports.createScheduleItem = async (req, res) => {
+    try {
+        const { eventId, title, description, startTime, endTime } = req.body;
+        const existingEvent = Event.findById(eventId);
+
+        if (!existingEvent)
+            return res.status(404).json({ message: 'Event not found' });
+
+        const scheduleItem = new ScheduleItem({
+            event: eventId, title, description, startTime, endTime
+        });
+
+        await scheduleItem.save();
+
+        existingEvent.scheduleItems.push(scheduleItem._id);
+
+        await existingEvent.save();
+
+        res.status(201).json({
+            message: 'Schedule item successfully',
+            data: { scheduleItem }
+        });
+    }
+    catch (err) {
+        res.status(500).json({ message: 'Internal server error' });
+    }
+}
